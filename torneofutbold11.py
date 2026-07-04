4c148ca815fae184efc178264f31f5bd97e01f15
Gabriel Aquino <aquinog261@gmail.com>
2026-07-04 08:44:03 -0300

    registro inicializado de los equipos

---
+++ torneofutbold11.py
@@ -0,0 +1,12 @@
+class Equipo:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.partidos_jugados = 0
+        self.partidos_ganados = 0
+        self.partidos_empatados = 0
+        self.partidos_perdidos = 0
+        self.goles_favor = 0
+        self.goles_contra = 0
+        self.diferencia_goles = 0
+        self.puntos = 0 
