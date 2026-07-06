438c1a24f7ff0e88ffb4f179054d9342884a994c
Gabriel Aquino <aquinog261@gmail.com>
2026-07-06 17:42:03 -0300

    agrego lo de buscar y registrar equipos, en un rato agrego el resto

--- torneofutbold11.py
+++ torneofutbold11.py
@@ -10,3 +10,29 @@ class Equipo:
         self.goles_contra = 0
         self.diferencia_goles = 0
         self.puntos = 0 
+
+def buscar_equipo(equipos, nombre):
+    for equipo in equipos:
+        if equipo.nombre.lower() == nombre.lower():
+            return equipo
+    return None
+
+
+def registrar_equipo(equipos):
+    if len(equipos) < 12:
+        nombre = input("Ingrese el nombre del equipo: ").strip()
+
+        while nombre == "" or buscar_equipo(equipos, nombre) is not None:
+            if nombre == "":
+                print("El nombre no puede estar vacío.")
+            else:
+                print("Ese equipo ya fue registrado.")
+
+            nombre = input("Ingrese otro nombre: ").strip()
+
+        nuevo_equipo = Equipo(nombre)
+        equipos.append(nuevo_equipo)
+
+        print("Equipo registrado correctamente.")
+    else:
+        print("Ya se registraron los 12 equipos.")
